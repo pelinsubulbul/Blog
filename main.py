68f5ad2e4d9940f62d3b6985831859d0f59c99e6
widdle99 <pelinsubulbul@hotmail.com>
2022-10-13 18:01:05 +0200

    BLOG4 LAST?

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def get_all_posts():
     posts = BlogPost.query.all()
     return render_template("index.html", all_posts=posts)
